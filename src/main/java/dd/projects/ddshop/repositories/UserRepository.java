785b64508caf8b0cfbda3bdaed9c97c94efddba2
Alin Stanciu <alin1stanciu@gmail.com>
2022-10-12 11:38:53 +0300

    Rename findByEmail method from UserRepository

--- src/main/java/dd/projects/ddshop/repositories/UserRepository.java
+++ src/main/java/dd/projects/ddshop/repositories/UserRepository.java
@@ -4,5 +4,5 @@ import dd.projects.ddshop.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-    User findByEmailIs(String email);
+    User findByEmail(String email);
 }
